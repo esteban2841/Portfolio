3b1d1f7d2dc4be9da8231e80192797144e108dd2
esteban2841 <rapuentes@uan.edu.co>
2023-11-09 13:45:38 -0500

    navitem created

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -1,10 +1,15 @@
-<template lang="eng">
-    <div>
-        
+<template lang="ts">
+    <div class="nav-links-container">
+        <h3>
+            <slot name="Link"></slot>
+        </h3>
     </div>
 </template>
 <script>
 </script>
 <style lang="">
-    
+    .nav-links-container{
+        display:flex;
+        background-color:blue;
+    }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+  import NavItem from './../components/NavItem.vue'
 </script>
 
 <template>
   <main>
+    <NavItem/>
   </main>
 </template>
