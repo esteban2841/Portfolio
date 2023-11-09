f7c454c502f275a79d0d03430b8d53a2a072eeaa
esteban2841 <rapuentes@uan.edu.co>
2023-11-09 13:24:52 -0500

    Adding Porfolio project created by Esteban using vue 3 boilerplate

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+
+  .wrapper {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+  }
+
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+}
+</style>
