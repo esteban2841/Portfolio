2d0e74c0cbe79bcb7096826c617c3778d434076d
esteban2841 <rapuentes@uan.edu.co>
2023-12-27 10:45:41 -0500

    foto uploaded

--- src/components/PhotoSection.vue
+++ src/components/PhotoSection.vue
@@ -1,20 +1,32 @@
 <template>
-    <div class="photo-container">
+    <div class="close-container">
         <span class="close-icon-container">
             <XIcon class="close-icon"/>
         </span>
     </div>
+    <div class="photo-section">
+        <div class="photo-mask">
+            <div class="photo-container">
+                <img src='require("../assets/foto-mask-left.png")'/>
+            </div>
+            <div></div>
+        </div>
+    </div>
+
 </template>
 <script setup lang="ts">
     import {XIcon} from 'heroicons-vue3/solid'
+    import photo from '../assets/foto-mask-left'
 </script>
 <style scoped>
-    .photo-container{
+    .close-container{
+        display: flex;
         background-color: #212121;
         padding: 10px;
         width: 100%;
         height: 60px;
         box-sizing: border-box;
+        justify-content: flex-end;
         .close-icon-container{
             width: 40px;
             height: 40px;
@@ -25,5 +37,20 @@
             width: 40px;
             height: 40px;
         }
+    }
+
+    .photo-section{
+        display: flex;
+        height: 142px;
+        padding: 10px;
+    }
+    .photo-mask{
+        background-color: aliceblue;
+        margin-top: 50px;
+    }
+    .photo-container{
+        width: 90px;
+        height: 90px;
+        background-color: aliceblue;
     }
 </style>
